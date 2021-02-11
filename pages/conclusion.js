615a0050b5cb6cfb4f29c31559096a379c0fe5de
kuwamura <naoyan418@gmail.com>
2021-02-12 07:27:11 +0900

    v1.0.2 まとめまとめ

--- pages/conclusion.js
+++ pages/conclusion.js
@@ -13,10 +13,21 @@ export default function Background() {
                     <H4>ビンゴアンケを、</H4>
                 </MyCard>
                 <MyCard sub>
-                    <H4>1. 開発した。</H4>
+                    <H4>1. 作って、</H4>
+                    <H5>・ 労力減らした</H5>
+                    <H5>・ 問題ゲーム要素を取り除いた</H5>
                 </MyCard>
                 <MyCard sub>
-                    <H4>2. 検証した。</H4>
+                    <H4>2. 試した。</H4>
+                    <H5>
+                        ・ ビンゴアンケ →<Blue>有効</Blue>
+                    </H5>
+                    <H5>
+                        ・ 回答値の質の低下 →<Blue>今回はなし</Blue>
+                    </H5>
+                    <H5>
+                        ・ ランキング →<Blue>有効かも</Blue>
+                    </H5>
                 </MyCard>
             </main>
         </>
